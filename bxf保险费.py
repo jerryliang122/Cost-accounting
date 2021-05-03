1efa3fff30cef2e8c9027b5111400fc5aa3de31e
jerryliang122 <jerryliang.chen@qq.com>
2021-05-03 14:49:00 +0800

    update bxf

--- bxf保险费.py
+++ bxf保险费.py
@@ -8,4 +8,7 @@ a = raw_input("1进口商/2出口商：")
 if a == "1":
     print('尚未支持')
 elif a == '2':
-    print('尚未支持')
+    toubaojine = 1.1 
+    #读取对外报价文件
+    file = open('cache/duiwaibaojia.txt') 
+    float(file.read())
