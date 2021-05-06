05f6e2cba55d6b8574f1786ed38c2232658fe2c4
jerryliang122 <jerryliang.chen@qq.com>
2021-05-06 15:01:57 +0800

    update yinhang

--- yinhang银行费用.py
+++ yinhang银行费用.py
@@ -2,3 +2,11 @@
 import os
 import math
 import time
+
+yinhangxuanze = input('票汇，电汇，信用证[1,2,3]：')
+if yinhangxuanze == "1":
+    pass
+elif yinhangxuanze == "2":
+    pass
+elif yinhangxuanze == "3":
+    pass
