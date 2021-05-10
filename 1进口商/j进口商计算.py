8380e2dbae32fdc625a4e0345ee9f81f2218b8e9
jerryliang122 <jerryliang.chen@qq.com>
2021-05-10 13:21:05 +0800

    update j

--- 1进口商/j进口商计算.py
+++ 1进口商/j进口商计算.py
@@ -6,12 +6,16 @@ import time
 mysy = input('贸易术语1FOB/2CRF/3CIF:')
 cjj =input('成交价:')
 cjsl = input('成交数量:')
-baozhuang = input('包装数量:')
-baozhuangzl=input('包装单件重量:')
-baozhuangtj=input('包装单件体积：')
-zl = float(baozhuangzl)/float(baozhuang)
-tj = float(baozhuangtj)/float(baozhuang)
-
+baozhuangsl = input('包装数量(转换到销售数量):')
+djbaozhuangzl=input('包装单件重量:')
+djbaozhuangtj=input('包装单件体积：')
+zl = float(djbaozhuangzl)/float(baozhuangsl)
+tj = float(djbaozhuangtj)/float(baozhuangsl)
+file1 = open('./cache/jk/baozhuangsl.txt','a')
+print>>file1,baozhuangsl
+file2 =open('./cache/jk/djbaozhuangzl.txt','a')
+print>>file2,baozhuangzl
+file3 =open('./cache/jk/djbaozhuangtj.txt','a')
 if mysy == '1':
     print('计算运费')
     time.sleep(2)
