90ef8657a252065b9d3885486ebc8817dd83e462
jerryliang122 <jerryliang.chen@qq.com>
2021-05-10 21:30:08 +0800

    update cbhs

--- cbhs成本核算.py
+++ cbhs成本核算.py
@@ -4,6 +4,8 @@ import time
 print('欢迎使用新版成本核算')
 print('正在初始化文件夹')
 os.system('rm -rf ./cache/*')
+os.system('mkdir ./cache/jk')
+os.system('mkdir ./cache/ck')
 choose = input('1进口商/2出口商:')
 if  choose == "1":
     print('正在启动进口商计算')
