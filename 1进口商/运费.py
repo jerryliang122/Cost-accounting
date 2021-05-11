9b613086e52c0e4dfd85ea1d3b3723c9b3b44a05
jerryliang122 <jerryliang.chen@qq.com>
2021-05-11 11:01:56 +0800

    update yunfei

--- 1进口商/运费.py
+++ 1进口商/运费.py
@@ -3,5 +3,12 @@ import os
 import math
 import time
 
-yh = input('1海运/2空运:')
-if yh =='1':
+yf =input('1海运/2空运:')
+
+if yf == "1":
+    pass
+elif yf == '2':
+    pass
+else:
+    print('输入错误')
+    os.system('python3 ./1进口商/运费.py')
