d3a6301f34f360842def7e9433dfad4fe7e7b81b
jerryliang122 <jerryliang.chen@qq.com>
2021-05-20 12:13:44 +0800

    新增采购成本

--- 2出口商/出口商计算.py
+++ 2出口商/出口商计算.py
@@ -8,23 +8,23 @@ file6 = open('./cache/ck/mysy.txt','a+')
 file6.write(mysy)
 file6.close()
 if mysy == '1':
-    os.system('python3 ./2出口商/对外报价.py')
     os.system('python3 ./2出口商/采购成本.py')
+    os.system('python3 ./2出口商/对外报价.py')
     os.system('python3./2出口商/国内费用.py')
     os.system('python3 ./2出口商/预期盈亏率.py')
     pass
 elif mysy == '2':
+    os.system('python3 ./2出口商/采购成本.py')
     os.system('python3 ./2出口商/对外报价.py')
     os.system('python3 ./2出口商/保险费.py')
-    os.system('python3 ./2出口商/采购成本.py')
     os.system('python3./2出口商/国内费用.py')
     os.system('python3 ./2出口商/预期盈亏率.py')
     pass
 else:
+    os.system('python3 ./2出口商/采购成本.py')
     os.system('python3 ./2出口商/对外报价.py')
     os.system('python3 ./2出口商/运费.py')
     os.system('python3 ./2出口商/保险费.py')
-    os.system('python3 ./2出口商/采购成本.py')
     os.system('python3./2出口商/国内费用.py')
     os.system('python3 ./2出口商/预期盈亏率.py')
     pass
