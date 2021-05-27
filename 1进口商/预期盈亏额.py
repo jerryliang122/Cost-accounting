03990079896ad478c7114f50f8156b7cb4c18eac
jerryliang <jerryliang.chen@qq.com>
2021-05-27 12:54:39 +0800

    修复预期盈亏额

--- 1进口商/预期盈亏额.py
+++ 1进口商/预期盈亏额.py
@@ -6,8 +6,16 @@ jksfhj = float(open('./cache/jk/jksfhj.txt').read())
 yhfyhj = float(open('./cache/jk/yhfyhj.txt').read())
 qtgnfyhj = float(open('./cache/jk/gnfyhy.txt').read())
 cif = float(open('./cache/jk/cif.txt').read())
-yfhj = float(open('./cache/jk/yf.txt').read())
-bxfhj = float(open('./cache/jk/bxf.txt').read())
+mysy = open('./cache/jk/mysy.txt').read()
+if mysy == '1':
+    yfhj = float(open('./cache/jk/yf.txt').read())
+    bxfhj = float(open('./cache/jk/bxf.txt').read())
+elif mysy == '2':
+    bxfhj = float(open('./cache/jk/bxf.txt').read())
+    yfhj = float(0)
+else:
+    bxfhj = float(0)
+    yfhj = float(0)
 zcb = cif + qtgnfyhj + yhfyhj + jksfhj + yfhj + bxfhj
 xhsl = float(open('./cache/jk/xhsl.txt').read())
 yqyke = xhsl - zcb
